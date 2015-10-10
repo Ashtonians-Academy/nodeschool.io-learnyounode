4e84bb74f78d658a4652baf42d4365bfa10eb834
Ashton Kinslow <github@ashtonkinslow.com>
2015-10-09 20:28:50 -0500

    Complete lesson 8

--- program.js
+++ program.js
@@ -1,8 +1,16 @@
 var http = require('http');
 
 http.get(process.argv[2], function (response) {
+    var streamResults =[];
+
     response.setEncoding('utf8');
     response.on('data',function(data){
-        console.log(data);
+        streamResults.push(data);
+    });
+
+    response.on('end',function(){
+        var results = streamResults.join('');
+        console.log(results.length);
+        console.log(results);
     });
 });
