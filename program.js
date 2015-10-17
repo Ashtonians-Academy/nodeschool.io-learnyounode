ce9e6d294374d9081514887c2de5bf7fb5da25dd
Ashton Kinslow <github@ashtonkinslow.com>
2015-10-17 10:41:00 -0500

    Complete Lesson 10

--- program.js
+++ program.js
@@ -1,28 +1,10 @@
-var http = require('http');
-var bl = require('bl');
+var net = require('net');
+var strftime = require('strftime');
 
-var response = [];
-var fetched = 0;
+var server = net.createServer(function(socket)
+{
+    socket.end(strftime('%F %R'));
+});
 
-function printResults () {
-  for (var i = 0; i < 3; i++)
-    console.log(response[i]);
-}
 
-function httpGet (i) {
-  http.get(process.argv[2 + i], function (responseRaw) {
-    responseRaw.pipe(bl(function (err, data) {
-      if (err)
-        return console.error(data);
-
-      response[i] = data.toString();
-      fetched++;
-
-      if (fetched === 3)
-        printResults();
-    }))
-  })
-}
-
-for (var i = 0; i < 3; i++)
-  httpGet(i);
+server.listen(process.argv[2]);
